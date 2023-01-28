8fb699d3265b588ed0fa9fa9e350bb37c183f2e2
Michael Jarvis <mjfullstack@gmail.com>
2023-01-28 23:47:36 +0000

    tests for reader and writer

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,3 +12,9 @@ def tests_path():
 def test_data_path(tests_path):
     return tests_path / "test_data"
 
+
+@pytest.fixture
+def valid_pdf_path(test_data_path):
+    return test_data_path / "test.pdf"
+
+
--- tests/test_file_operations.py
+++ tests/test_file_operations.py
@@ -12,6 +12,11 @@ TEST_INVALID_FILE = "invalid.pdf_"
 OUTPUT_FILE = "output.pdf"
 
 
+@pytest.fixture
+def output_path(test_data_path):
+    return test_data_path / OUTPUT_FILE
+
+
 @pytest.mark.parametrize(
     "filename", [TEST_VALID_FILE, TEST_FILE_NOT_FOUND, TEST_INVALID_FILE]
 )
@@ -27,16 +32,29 @@ def test_load_file(test_data_path, filename):
             pdf = load_pdf(full_path)
 
 
-def test_write_pdf_reader_to_file(test_data_path):
-    pdf = PdfReader(test_data_path / TEST_VALID_FILE)
-    output_path = test_data_path / OUTPUT_FILE
+def test_write_pdf_reader_to_file(test_data_path, valid_pdf_path, output_path):
+    pdf = PdfReader(valid_pdf_path)
     assert not os.path.exists(output_path)
     write_pdf_to_file(pdf, output_path)
     assert os.path.exists(output_path)
     os.remove(output_path)
 
 
-def test_write_pdf_writer_to_file(test_data_path):
-    pdf = PdfWriter(test_data_path / TEST_VALID_FILE)
+def test_write_pdf_writer_to_file(test_data_path, valid_pdf_path, output_path):
+    pdf = PdfWriter(valid_pdf_path)
+    assert not os.path.exists(output_path)
+    write_pdf_to_file(pdf, output_path)
+    assert os.path.exists(output_path)
+    os.remove(output_path)
+
+
+def test_write_pdf_reader_to_bytes(valid_pdf_path):
+    pdf = PdfReader(valid_pdf_path)
+    pdf_b = write_pdf_to_bytes(pdf)
+    assert isinstance(pdf_b, bytes)
+
+
+def test_write_pdf_writer_to_bytes(valid_pdf_path):
+    pdf = PdfWriter(valid_pdf_path)
     pdf_b = write_pdf_to_bytes(pdf)
     assert isinstance(pdf_b, bytes)
