ecc2e8e4184218d358bf26efa248937ac62bd879
Michael Jarvis <mjfullstack@gmail.com>
2023-01-28 23:29:12 +0000

    tested working read/write operations

--- pdf_form/file_operations.py
+++ pdf_form/file_operations.py
@@ -1,8 +1,31 @@
-def load_pdf(filename):
-    if not str(filename).endswith('.pdf'):
-        raise ValueError("file must be pdf file with ext '.pdf'")
-    with open(filename) as f:
-        f.read()
+import io
 
-def write_pdf(filename):
-    pass
+from pypdf import PdfReader, PdfWriter
+
+
+def load_pdf(filename):
+    return PdfReader(filename)
+
+
+def _get_writer_from_pdf_object(pdf):
+    if isinstance(pdf, PdfReader):
+        writer = PdfWriter()
+        writer.clone_document_from_reader(pdf)
+    elif isinstance(pdf, PdfWriter):
+        writer = pdf
+    else:
+        raise TypeError("pdf must be a PdfReader or PdfWriter")
+    return writer
+
+
+def write_pdf_to_file(pdf, filename):
+    writer = _get_writer_from_pdf_object(pdf)
+    with open(filename, "wb") as f:
+        writer.write(f)
+
+
+def write_pdf_to_bytes(pdf):
+    writer = _get_writer_from_pdf_object(pdf)
+    with io.BytesIO() as buffer:
+        writer.write(buffer)
+        return buffer.getvalue()
--- pdf_form/tests/test_file_operations.py
+++ pdf_form/tests/test_file_operations.py
@@ -1,10 +1,15 @@
-import pytest
+import os
 
-from pdf_form.file_operations import load_pdf, write_pdf
+import pypdf.errors
+import pytest
+from pypdf import PdfReader, PdfWriter
+
+from pdf_form.file_operations import load_pdf, write_pdf_to_file, write_pdf_to_bytes
 
 TEST_VALID_FILE = "test.pdf"
 TEST_FILE_NOT_FOUND = "_test.pdf"
-TEST_INVALID_FILE = "invalid.pdf"
+TEST_INVALID_FILE = "invalid.pdf_"
+OUTPUT_FILE = "output.pdf"
 
 
 @pytest.mark.parametrize(
@@ -17,6 +22,20 @@ def test_load_file(tests_path, filename):
     elif filename == TEST_FILE_NOT_FOUND:
         with pytest.raises(FileNotFoundError):
             pdf = load_pdf(full_path)
-    else:
-        with pytest.raises(Exception):
+    elif filename == TEST_INVALID_FILE:
+        with pytest.raises(pypdf.errors.EmptyFileError):
             pdf = load_pdf(full_path)
+
+
+def test_write_pdf_reader_to_file(tests_path):
+    pdf = PdfReader(tests_path / TEST_VALID_FILE)
+    assert not os.path.exists(tests_path / OUTPUT_FILE)
+    write_pdf_to_file(pdf, tests_path / OUTPUT_FILE)
+    assert os.path.exists(tests_path / OUTPUT_FILE)
+    os.remove(tests_path / OUTPUT_FILE)
+
+
+def test_write_pdf_writer_to_file(tests_path):
+    pdf = PdfWriter(tests_path / TEST_VALID_FILE)
+    pdf_b = write_pdf_to_bytes(pdf)
+    assert isinstance(pdf_b, bytes)
