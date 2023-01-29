6d00a5a9917783841a2bd6c1e01293cbc4a5c2de
Michael Jarvis <mjfullstack@gmail.com>
2023-01-29 00:19:17 +0000

    run tests script / mypy

--- manage.py
+++ manage.py
@@ -1,2 +1,44 @@
 #!/usr/bin/env python3
+import subprocess
+import time
 
+import click
+
+
+def wait_for_logs(cmd, msg):
+    logs = subprocess.check_output(cmd)
+    while msg not in logs.decode('utf-8'):
+        time.sleep(0.1)
+        logs = subprocess.check_output(cmd)
+
+
+@click.group()
+def tests():
+    pass
+
+
+@tests.command()
+def pytest():
+    print("Running all tests")
+    subprocess.call(["pytest"])
+
+
+@tests.command()
+def mypy():
+    print("Running mypy")
+    subprocess.call(["mypy", "."])
+
+
+@tests.command()
+def run_tests():
+    print("Unit tests:\n")
+    cmd = ["pytest"]
+    subprocess.call(cmd)
+    wait_for_logs(cmd, "test session starts")
+    print("\n")
+    print("Type checking:\n")
+    subprocess.call(["mypy", "."])
+
+
+if __name__ == "__main__":
+    tests()
--- pdf_form/file_operations.py
+++ pdf_form/file_operations.py
@@ -1,13 +1,18 @@
 import io
+from pathlib import Path
+from typing import Union
 
 from pypdf import PdfReader, PdfWriter
 
 
-def load_pdf(filename):
+GenericPdfType = Union[PdfReader, PdfWriter]
+
+
+def load_pdf(filename: Union[str, Path]) -> PdfReader:
     return PdfReader(filename)
 
 
-def _get_writer_from_pdf_object(pdf):
+def _get_writer_from_pdf_object(pdf: GenericPdfType) -> PdfWriter:
     if isinstance(pdf, PdfReader):
         writer = PdfWriter()
         writer.clone_document_from_reader(pdf)
@@ -18,13 +23,13 @@ def _get_writer_from_pdf_object(pdf):
     return writer
 
 
-def write_pdf_to_file(pdf, filename):
+def write_pdf_to_file(pdf: GenericPdfType, filename) -> None:
     writer = _get_writer_from_pdf_object(pdf)
     with open(filename, "wb") as f:
         writer.write(f)
 
 
-def write_pdf_to_bytes(pdf):
+def write_pdf_to_bytes(pdf: GenericPdfType) -> bytes:
     writer = _get_writer_from_pdf_object(pdf)
     with io.BytesIO() as buffer:
         writer.write(buffer)
